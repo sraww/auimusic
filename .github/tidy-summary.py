1c9d3462465f4ec7963927ce83421f73fc7e5d9d
Alex2772 <alex2772sc@gmail.com>
2025-06-17 15:15:18 +0300

    Create LICENSE (#6)
    * Create LICENSE
    
    Closes #5
    
    * added license headers
    
    * upadaet vesiron

--- .github/tidy-summary.py
+++ .github/tidy-summary.py
@@ -1,11 +1,21 @@
-#  AUI Framework - Declarative UI toolkit for modern C++20
-#  Copyright (C) 2020-2024 Alex2772 and Contributors
+#  AUImusic, a music player based on AUI Framework
+#  Copyright (C) 2025 Alex2772
 #
-#  SPDX-License-Identifier: MPL-2.0
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
 #
-#  This Source Code Form is subject to the terms of the Mozilla Public
-#  License, v. 2.0. If a copy of the MPL was not distributed with this
-#  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#  SPDX-License-Identifier: GPL-3.0
+
 
 """
 
